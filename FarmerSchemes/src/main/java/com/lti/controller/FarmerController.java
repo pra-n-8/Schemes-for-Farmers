35bfc0ac7d3e853ed2c9de8c99cef4349df48243
pra-n-8 <pra-n-8@users.noreply.github.com>
2019-11-23 14:42:54 -0800

    Session

--- FarmerSchemes/src/main/java/com/lti/controller/FarmerController.java
+++ FarmerSchemes/src/main/java/com/lti/controller/FarmerController.java
@@ -1,5 +1,7 @@
 package com.lti.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
@@ -7,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.lti.interfaces.GenericInterface;
 import com.lti.interfaces.RegistrationLoginInterface;
 import com.lti.model.Farmer;
 
@@ -23,12 +24,13 @@ public class FarmerController {
 	}
 
 	@RequestMapping(path = "farmerlogin.lti", method = RequestMethod.POST)
-	public String loginFarmer(@RequestParam(name = "username") String username,@RequestParam(name = "pass") String password) {
+	public String loginFarmer(@RequestParam(name = "username") String username,@RequestParam(name = "pass") String password,HttpSession session) {
 		Farmer farmer;
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("app-config.xml");
 		RegistrationLoginInterface si = (RegistrationLoginInterface) ctx.getBean("farmerService");
 		try {
 		farmer=(Farmer)si.login(username,password);
+		session.setAttribute("fname", farmer.getFarmerName());
 		}
 		catch(NullPointerException e){
 			System.out.println(e);
---
+++ FarmerSchemes/src/main/webapp/home.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	
+	<%
+		if(session.getAttribute("fname")!=null){
+			out.println("Welcome "+session.getAttribute("fname"));
+		}
+	%>
+</body>
+</html>
